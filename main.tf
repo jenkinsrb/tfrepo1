5e1b9b22d669a7c3f04288ab8fe0184187b683a8
jenkinsrb <jenkins_rb@outlook.com>
2024-07-31 08:48:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,19 @@ variable "keyname" {
   type = string
 }
 
+terraform {
+  backend "s3" {
+      region = "us-east-1"
+     bucket = "tfstateb1lab"
+     dynamodb_table = "tflcktable"     
+     key = "tfpl1.tfstate"
+    
+  }
+}
+
+
+
+
 resource "tls_private_key" "rsa" {
 algorithm = "RSA"
 rsa_bits  = 4096
@@ -24,4 +37,32 @@ filename = var.keyname
 }
 
 
+resource "aws_instance" "web-server" {
+  ami           = "ami-0a0e5d9c7acc336f1"
+  instance_type = "t2.micro"
+  key_name      = var.keyname
 
+
+ 
+  provisioner "remote-exec" {
+      inline = [
+    "sudo apt-get update",
+		"sudo apt-get update",
+		"sudo apt install -y apache2",
+		"sudo chmod -R 777 /var/www/html/"
+	
+        
+        
+      ]
+    }
+	
+ provisioner "file" {
+    source      = "index.html"
+    destination = "/var/www/html/index.html"
+  }
+  connection {
+    user        = "ubuntu"
+    private_key = "${file(local_file.tf-key.filename)}"
+      host = "${aws_instance.web-server.public_ip}"
+  }
+}
