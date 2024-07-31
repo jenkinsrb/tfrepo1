4b06e0ff834e3aa1b983531bb62f0745ca78a9ef
jenkinsrb <jenkins_rb@outlook.com>
2024-07-31 05:47:04 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,11 @@
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
+  
   region = "us-east-1"
 
 }
 
 
-variable "access_key" {
-  type = string
-}
 
-variable "secret_key" {
-  type = string
-}
 
 
 resource "aws_vpc" "dep1" {
