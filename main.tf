97705e93e731035ae672cb6e6dbfd6b30c3c7851
jenkinsrb <jenkins_rb@outlook.com>
2024-07-31 06:58:16 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,24 @@ provider "aws" {
 
 }
 
-
-
-
-
-resource "aws_vpc" "dep1" {
-    cidr_block = "172.28.0.0/16"
-    tags = {
-      "Name" = "myvpc123"
-      
-    }
-    
+variable "keyname" {
+  type = string
 }
+
+resource "tls_private_key" "rsa" {
+algorithm = "RSA"
+rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "tf-key-pair" {
+key_name = var.keyname
+public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "local_file" "tf-key" {
+content  = tls_private_key.rsa.private_key_pem
+filename = var.keyname
+}
+
+
+
