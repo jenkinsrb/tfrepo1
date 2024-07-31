a20bd7986e6badc7e7be91ab759154768a751816
jenkinsrb <jenkins_rb@outlook.com>
2024-07-31 05:29:07 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
-provider "aws" { 
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
   region = "us-east-1"
+
 }
+
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
 
 resource "aws_vpc" "dep1" {
     cidr_block = "172.28.0.0/16"
